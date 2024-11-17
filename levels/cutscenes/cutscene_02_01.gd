fdb4703eee9dc6c6fdec65bb5dfba14c5ae59509
Alex Piotrowski <apiotrow@apiotrow-thinkpadt14gen3.remote.csb>
2024-11-17 22:59:58 +1100

    will export now for test

--- levels/cutscenes/cutscene_02_01.gd
+++ levels/cutscenes/cutscene_02_01.gd
@@ -4,6 +4,6 @@ extends Node2D
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "zozo_call":
-		get_tree().change_scene_to_file("res://levels/scene_02.tscn")
+		get_tree().change_scene_to_file("res://levels/scene_02_01.tscn")
 		GlobalMusicManager.load_music("res://sounds/music/Rainoth Unmissed.mp3")
 		GlobalMusicManager.play()
